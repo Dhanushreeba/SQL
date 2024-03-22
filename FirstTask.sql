74d2c5dbb1eff5bb68a4c9b3590eaf7b9d47f230
Dhanushreeba <dhanushreeba.xworkz@gmail.comgit config --global user.name Dhanushreeba git config --global user.email dhanushreeba.xworkz@gmail.com>
2024-03-22 08:08:27 +0530

    sql task

---
+++ FirstTask.sql
@@ -0,0 +1,11 @@
+CREATE TABLE table_name(id int,name varchar(30),price bigint);
+
+CREATE TABLE table1_name(id int,song varchar(246),movie bigint);
+
+CREATE TABLE cloth_name(id int,brand varchar(23),material bigint);
+
+CREATE TABLE chair_name(id int,height varchar(12),flexible bigint);
+
+CREATE TABLE bottle_name(id int,color varchar(10),size bigint);
+
+CREATE TABLE swicth_name(id int,type varchar(5),pin bigint);
