7574f75e8ba612861cd9ec82b761ea4634a477fd
Dhanushreeba <dhanushreeba.xworkz@gmail.comgit config --global user.name Dhanushreeba git config --global user.email dhanushreeba.xworkz@gmail.com>
2024-03-25 20:16:15 +0530

    insert database

---
+++ Thirdtask(insert).sql
@@ -0,0 +1,61 @@
+SELECT * FROM table_name;
+
+INSERT INTO table_name values(1,"flex",2000,"Ex12",3200);
+INSERT INTO table_name values(2,"Banner",3000,"fg12",400);
+INSERT INTO table_name values(3,"Wood",4000,"Xz12",1600);
+INSERT INTO table_name values(4,"Table",10000,"qs12",1200);
+INSERT INTO table_name values(5,"Steel",12000,"hg10",8900);
+INSERT INTO table_name values(6,"Fiber",7000,"kl12",300);
+INSERT INTO table_name values(7,"Metal",6000,"Li12",6700);
+INSERT INTO table_name values(8,"Clor",1000,"po62",5600);
+INSERT INTO table_name values(9,"Sheet",6780,"as98",1300);
+INSERT INTO table_name values(10,"gheet",9087,"cv76",7800);
+
+SELECT * FROM bottle_name;
+
+INSERT INTO bottle_name values(1,"Green",56709,20,"56a","ak",60000);
+INSERT INTO bottle_name values(2,"Red",23409,21,"76k","jh6",12000);
+INSERT INTO bottle_name values(3,"Yellow",87509,22,"43F","hh",50000);
+INSERT INTO bottle_name values(4,"black",56092,23,"98GF","ff",40000);
+INSERT INTO bottle_name values(5,"blue",12509,24,"90NB","xx",70000);
+INSERT INTO bottle_name values(6,"orange",33467,25,"fd34","uy",90000);
+INSERT INTO bottle_name values(7,"skyBlue",54719,26,"tv243","as",20000);
+INSERT INTO bottle_name values(8,"grey",39705,27,"89mn","er",56000);
+INSERT INTO bottle_name values(9,"voilet",96819,28,"bf98","dr",69000);
+INSERT INTO bottle_name values(10,"purpel",76543,29,"23jk","nbv",11000);
+
+SELECT * FROM chair_name;
+INSERT INTO chair_name values(1,"45inch",987,"NeelKamal",1000);
+INSERT INTO chair_name values(2,"90inch",87493,"good",2000);
+INSERT INTO chair_name values(3,"85inch",9901,"verygood",3000);
+INSERT INTO chair_name values(4,"42inch",934,"best",4000);
+INSERT INTO chair_name values(5,"87inch",187,"better",5000);
+INSERT INTO chair_name values(6,"76inch",587,"not bad",6000);
+INSERT INTO chair_name values(7,"34inch",967,"bad",7000);
+INSERT INTO chair_name values(8,"12inch",9977,"verybad",8000);
+INSERT INTO chair_name values(9,"98inch",956,"poor",9000);
+INSERT INTO chair_name values(10,"10inch",9546,"notgood",1900);
+
+SELECT * FROM cloth_name;
+INSERT INTO cloth_name values(1,"avasa",23,423,6789);
+INSERT INTO cloth_name values(2,"akrithi",24,424,6781);
+INSERT INTO cloth_name values(3,"gocolor",25,425,6782);
+INSERT INTO cloth_name values(4,"zivame",26,426,6783);
+INSERT INTO cloth_name values(5,"hnm",27,427,6784);
+INSERT INTO cloth_name values(6,"zudio",28,428,6785);
+INSERT INTO cloth_name values(7,"max",29,429,6786);
+INSERT INTO cloth_name values(8,"nike",30,421,6787);
+INSERT INTO cloth_name values(9,"gap",31,422,6788);
+INSERT INTO cloth_name values(10,"teamspirit",32,430,6767);
+
+SELECT * FROM swicth_name;
+INSERT INTO swicth_name values(1,"wall",12,345,4567);
+INSERT INTO swicth_name values(2,"steel",13,341,4570);
+INSERT INTO swicth_name values(3,"fiber",14,342,4569);
+INSERT INTO swicth_name values(4,"plast",15,343,4568);
+INSERT INTO swicth_name values(5,"metal",16,344,4561);
+INSERT INTO swicth_name values(6,"brick",17,346,4562);
+INSERT INTO swicth_name values(7,"cloth",18,347,4563);
+INSERT INTO swicth_name values(8,"bang",19,348,4564);
+INSERT INTO swicth_name values(9,"wood",11,349,4565);
+INSERT INTO swicth_name values(10,"sad",20,350,4566);
