03af4f1a58cd469e199ad282c81e4e0886e436b8
Dhanushreeba <dhanushreeba.xworkz@gmail.comgit config --global user.name Dhanushreeba git config --global user.email dhanushreeba.xworkz@gmail.com>
2024-03-27 08:17:53 +0530

    insert

---
+++ dummy(insert,modify).sql
@@ -0,0 +1,13 @@
+SELECT * FROM table1_name;
+
+ALTER TABLE table1_name MODIFY COLUMN feature_name int;
+
+INSERT INTO table1_name values(1,"lyric",5462,"abc",8);
+INSERT INTO table1_name values(2,"music",5463,"abcd",3);
+INSERT INTO table1_name values(3,"patho",5464,"abd",7);
+INSERT INTO table1_name values(4,"rap",5465,"ada",9);
+
+ALTER TABLE table1_name MODIFY COLUMN feature_name int;
+
+SELECT * FROM table1_name where feature_name = '7';
+
